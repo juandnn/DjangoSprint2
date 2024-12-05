240940e11fe7e9932be1c74bdd5274cb37fb4095
Juan Diego Niebles Navas <diego.niebles@hotmail.com>
2024-12-05 10:27:58 -0500

    basedatos

--- ofipensiones/usuarios/models.py
+++ ofipensiones/usuarios/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Usuario(models.Model):
     username = models.CharField(max_length=100)
     clave = models.CharField(max_length=100)
+
     
     def __str__(self):
         return self.username 
